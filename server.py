ec6f6ab94dc2c64b524d019e00d508c53c2b2b50
M-Nikhil20 <nanim200904@gmail.com>
2023-01-06 13:33:06 +0530

    added image

---
+++ server.py
@@ -0,0 +1,28 @@
+from http.server import HTTPServer, BaseHTTPRequestHandler
+content = """
+<!DOCTYPE html>
+<html>
+<head>
+<title>My webserver</title>
+</head>
+<body>
+<h1>Welcome<h1>
+<h2>Name: Nikhil M<h2>
+<h3>Ref: 22008584<h3>
+<h4>Top Web Application Developoment Framework<h4>
+<h5>1- Django<h5>
+<h6>2. React<h6>
+</body>
+</html>
+"""
+class myhandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        print("request received")
+        self.send_response(200)
+        self.send_header('content-type', 'text/html; charset=utf-8')
+        self.end_headers()
+        self.wfile.write(content.encode())
+server_address = ('',80)
+httpd = HTTPServer(server_address,myhandler)
+print("my webserver is running...")
+httpd.serve_forever()
